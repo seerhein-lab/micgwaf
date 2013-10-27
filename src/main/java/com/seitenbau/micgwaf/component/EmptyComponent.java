cd4986bfd3e56b3907f0b88113be1d4797d97be9
Thomas Fox <fox@seitenbau.de>
2013-10-27 20:40:58 +0100

    Add javadoc and Serial Version UID.

--- src/main/java/com/seitenbau/micgwaf/component/EmptyComponent.java
+++ src/main/java/com/seitenbau/micgwaf/component/EmptyComponent.java
@@ -10,11 +10,25 @@ import java.util.List;
  */
 public class EmptyComponent extends Component
 {
+  /** Serial Version UID. */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Constructor for an empty Component without id.
+   * 
+   * @param parent the parent component.
+   */
   public EmptyComponent(Component parent)
   {
     super(null, parent);
   }
 
+  /**
+   * Constructor for an empty Component.
+   * 
+   * @param id the id of the component.
+   * @param parent the parent component.
+   */
   public EmptyComponent(String id, Component parent)
   {
     super(id, parent);
