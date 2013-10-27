1b52c49e34d51ee347ce5b02846d8a8cacfa48cc
Thomas Fox <fox@seitenbau.de>
2013-10-27 20:54:11 +0100

    add javadoc and serialVersionUID

--- src/main/java/com/seitenbau/micgwaf/component/GenerationParameters.java
+++ src/main/java/com/seitenbau/micgwaf/component/GenerationParameters.java
@@ -2,8 +2,14 @@ package com.seitenbau.micgwaf.component;
 
 import java.io.Serializable;
 
+/**
+ * Contains parameters for a component which are used in generation time but not in runtime.
+ */
 public class GenerationParameters implements Serializable
 {
+  /** Serial Version UID. */
+  private static final long serialVersionUID = 1L;
+  
   /**
    * If true, a pair of classes (base and extension) should be generated for the component, if possible.
    * If false, only one class should be generated.
