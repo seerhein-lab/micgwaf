ae36dfc30f68e98fa0a9bc77c608e0135584d927
Thomas Fox <fox@seitenbau.de>
2014-01-17 19:48:11 +0100

    add javadoc

--- micgwaf-core/src/main/java/com/seitenbau/micgwaf/util/Assertions.java
+++ micgwaf-core/src/main/java/com/seitenbau/micgwaf/util/Assertions.java
@@ -2,6 +2,15 @@ package com.seitenbau.micgwaf.util;
 
 public class Assertions
 {
+  /**
+   * Checks that an object is not null.
+   * 
+   * @param toCheck the object to check.
+   * @param name the name of the object, which is referenced in a NullPointerException
+   *        if the check fails.
+   *
+   * @throws NullPointerException if toCheck is null.
+   */
   public static void assertNotNull(Object toCheck, String name)
   {
     if (toCheck == null)
