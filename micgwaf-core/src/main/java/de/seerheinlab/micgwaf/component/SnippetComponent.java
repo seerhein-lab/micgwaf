5d5482c3f0f4f012b2df920cee7ee3f3d64a3f33
Thomas Fox <fox@seitenbau.de>
2014-04-26 22:03:41 +0200

    javadoc, remove unchanged overriding method

--- micgwaf-core/src/main/java/de/seerheinlab/micgwaf/component/SnippetComponent.java
+++ micgwaf-core/src/main/java/de/seerheinlab/micgwaf/component/SnippetComponent.java
@@ -5,7 +5,6 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * A component representing a fixed html snippet.
@@ -18,22 +17,35 @@ public class SnippetComponent extends Component
   /** The referenced snippet. */
   public String snippet;
   
+  /**
+   * Constructor.
+   * 
+   * @param parent the parent component. May be null if this is a standalone component (e.g. a page).
+   */
   public SnippetComponent(Component parent)
   {
     super(null, parent);
   }
 
+  /**
+   * Constructor. 
+   * 
+   * @param id the id of the component, may be null. The id is not used in this component.
+   * @param parent the parent component. May be null if this is a standalone component (e.g. a page).
+   */
   public SnippetComponent(String id, String snippet, Component parent)
   {
     super(id, parent);
     this.snippet = snippet;
   }
   
-  public void resolveComponentReferences(Map<String, ? extends Component> allComponents)
-  {
-    super.resolveComponentReferences(allComponents);
-  }
-  
+  /**
+   * Returns the list of children of this component.
+   * This component has no children and therefore returns an empty unmodifiable list.
+   * 
+   * @return the list of children, not null.
+   */
+  @Override
   public List<Component> getChildren()
   {
     List<Component> result = Collections.unmodifiableList(new ArrayList<Component>());
