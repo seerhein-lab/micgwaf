54b8dfb854164cbde0b884519f0ec716fb7d8c31
Thomas Fox <fox@seitenbau.de>
2014-04-28 08:15:52 +0200

    Rename SnippetListComponent to PartListComponent

---
+++ micgwaf-core/src/main/java/de/seerheinlab/micgwaf/component/PartListComponent.java
@@ -0,0 +1,167 @@
+package de.seerheinlab.micgwaf.component;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import de.seerheinlab.micgwaf.util.Assertions;
+
+/**
+ * A component representing a list of HTML snippets or other components (called parts).
+ * This component is mainly used during parsing HTML documents.
+ */
+public class PartListComponent extends Component
+{
+  /** Serial Version UID. */
+  private static final long serialVersionUID = 1L;
+
+  /** The list of parts. */
+  public final List<ComponentPart> parts;
+  
+  /**
+   * Constructs a SnippetListComponent containing no parts.
+   * 
+   * @param parent the parent component. May be null if this is a standalone component (e.g. a page).
+   */
+  public PartListComponent(Component parent)
+  {
+    super(null, parent);
+    this.parts = new ArrayList<>();
+  }
+
+  /**
+   * Constructs a SnippetListComponent with an id, containing no parts.
+   * 
+   * @param id the id of the component, may be null. The id is not used in this component.
+   * @param parent the parent component. May be null if this is a standalone component (e.g. a page).
+   */
+  public PartListComponent(String id, Component parent)
+  {
+    super(id, parent);
+    this.parts = new ArrayList<>();
+  }
+
+  /**
+   * Constructs a SnippetListComponent with the given initial parts.
+   * 
+   * @param parts the initial parts of this component, not null. This list is copied, but it contents
+   *        are not cloned in any way.
+   * @param parent the parent component. May be null if this is a standalone component (e.g. a page).
+   */
+  public PartListComponent(List<ComponentPart> parts, Component parent)
+  {
+    super(null, parent);
+    Assertions.assertNotNull(parts, "parts");
+    this.parts = new ArrayList<>(parts);
+  }
+  
+  public void resolveComponentReferences(Map<String, ? extends Component> allComponents)
+  {
+    super.resolveComponentReferences(allComponents);
+    for (ComponentPart part : parts)
+    {
+      if (part.component != null)
+      {
+        part.component.resolveComponentReferences(allComponents);
+      }
+    }
+  }
+  
+  public List<Component> getChildren()
+  {
+    List<Component> result = new ArrayList<>();
+    for (ComponentPart part : parts)
+    {
+      if (part.component != null)
+      {
+        result.add(part.component);
+      }
+    }
+    return result;
+  }
+
+  @Override
+  public void render(Writer writer) throws IOException
+  {
+    for (ComponentPart part : parts)
+    {
+      if (part.htmlSnippet != null)
+      {
+        writer.write(part.htmlSnippet);
+      }
+      else if (part.component != null)
+      {
+        part.component.render(writer);
+      }
+      else
+      {
+        throw new IllegalStateException(
+            "either htmlSnippet or component must be set on part " + part);
+      }
+    }
+  }
+  
+  /**
+   * Returns a shallow copy of this object.
+   * The part list is copied and thus may be changed in the copy without affecting the copy origin.
+   * However, the referenced components (the parts and the parent reference) are not copied.
+   * 
+   * @return a shallow copy of this object, not null.
+   */
+  public PartListComponent copy()
+  {
+    PartListComponent result = new PartListComponent(parent);
+    for (ComponentPart part : parts)
+    {
+      result.parts.add(part.clone());
+    }
+    return result;
+  }
+  
+  /**
+   * A part of the SnippetList component.
+   * Contains either a HTML Snippet or a child component.
+   */
+  public static class ComponentPart implements Cloneable
+  {
+    public String htmlSnippet;
+    
+    public Component component;
+    
+    public static ComponentPart fromHtmlSnippet(String htmlSnippet)
+    {
+      ComponentPart result = new ComponentPart();
+      result.htmlSnippet = htmlSnippet;
+      return result;
+    }
+    
+    public ComponentPart clone()
+    {
+      try
+      {
+        return (ComponentPart) super.clone();
+      }
+      catch (CloneNotSupportedException e)
+      {
+        // should not happen as we implement Cloneaable
+        throw new RuntimeException(e);
+      }
+    }
+
+    public static ComponentPart fromComponent(Component component)
+    {
+      ComponentPart result = new ComponentPart();
+      result.component = component;
+      return result;
+    }
+    
+    @Override
+    public String toString()
+    {
+      return "ComponentPart [htmlSnippet="
+          + htmlSnippet + ", component=" + component + "]";
+    }
+  }
+}
