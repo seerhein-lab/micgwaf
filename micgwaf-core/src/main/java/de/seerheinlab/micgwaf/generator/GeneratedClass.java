23d26f6d5fcf6bcea5b2c7ce49a709a3859567a2
Thomas Fox <fox@seitenbau.de>
2014-07-05 21:10:45 +0200

    add missing files for last commit

---
+++ micgwaf-core/src/main/java/de/seerheinlab/micgwaf/generator/GeneratedClass.java
@@ -0,0 +1,67 @@
+package de.seerheinlab.micgwaf.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratedClass
+{
+  public String classPackage = "";
+  
+  public List<String> imports = new ArrayList<>();
+  
+  public StringBuilder classJavadoc = new StringBuilder();
+  
+  public List<String> classAnnotations = new ArrayList<>();
+  
+  public StringBuilder classDefinition = new StringBuilder();
+
+  public StringBuilder classBody = new StringBuilder();
+  
+  public List<GeneratedClass> innerClasses = new ArrayList<>();
+  
+  public String toString()
+  {
+    StringBuilder result = new StringBuilder();
+    result.append("package ").append(classPackage).append(";\n\n");
+    for (String toImport : calculateImports())
+    {
+      result.append("import ").append(toImport).append(";\n");
+    }
+    result.append(";\n");
+    appendMainPart(result, 0);
+    return result.toString();
+  }
+  
+  public void appendMainPart(StringBuilder result, int indent)
+  {
+    result.append(GeneratorHelper.indent(classJavadoc.toString(), indent)).append("\n");
+    for (String classAnnotation : classAnnotations)
+    {
+      result.append(GeneratorHelper.indent(classAnnotation, indent)).append("\n");
+    }
+    result.append(GeneratorHelper.indent(classDefinition.toString(), indent)).append("\n");
+    result.append(GeneratorHelper.indent("{\n", indent));
+    result.append(GeneratorHelper.indent(classBody.toString(), indent)).append("\n");
+    for (GeneratedClass innerClass : innerClasses)
+    {
+      innerClass.appendMainPart(result, indent + 2);
+    }
+    result.append(GeneratorHelper.indent("}\n", indent));
+  }
+  
+  public List<String> calculateImports()
+  {
+    List<String> result = new ArrayList<>(imports);
+    for (GeneratedClass innerClass : innerClasses)
+    {
+      for (String toImport : innerClass.calculateImports())
+      {
+        if (!result.contains(toImport))
+        {
+          result.add(toImport);
+        }
+      }
+    }
+    return result;
+  }
+}
---
+++ micgwaf-core/src/main/java/de/seerheinlab/micgwaf/generator/GeneratorHelper.java
@@ -0,0 +1,39 @@
+package de.seerheinlab.micgwaf.generator;
+
+/**
+ * Helper methods for generators.
+ */
+public class GeneratorHelper
+{
+  /**
+   * Returns the indent String for an indent number.
+   * 
+   * @param indent the indent index.
+   * 
+   * @return the corresponding indent String, not null.
+   */
+  public static String getIndentString(int indent)
+  {
+    // TODO cache result
+    StringBuilder result = new StringBuilder(indent);
+    for (int i = 0; i < indent; ++i)
+    {
+      result.append(" ");
+    }
+    return result.toString();
+  }
+  
+  /**
+   * Indents a String with the given number of spaced.
+   * 
+   * @param toIndent the String to indent, not null.
+   * @param indent the indent, not null.
+   * 
+   * @return the indented String.
+   */
+  public static String indent(String toIndent, int indent)
+  {
+    String indentString = getIndentString(indent);
+    return indentString + toIndent.replace("\n", "\n"+ indentString);
+  }
+}
